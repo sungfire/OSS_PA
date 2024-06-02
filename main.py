807b49a9316e7a4eecdecd656b9c42d2d29f43db
LSH <dlatjdgns424@naver.com>
2024-06-02 22:04:29 +0900

    prepare player image, make player class and def'init'

--- main.py
+++ main.py
@@ -13,10 +13,24 @@ GREEN = (0, 255 , 0)
 RED = (255, 0, 0)
 
 # set the size of screen & display screen
-size = [500,400]
+width = 600
+height = 900
+size = [width, height]
 screen = pg.display.set_mode(size)
 
 pg.display.set_caption("Infinite_stairs")
+
+player_image = pg.image.load('image/image.jpg')
+
+# set player class
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pg.transform.scale(player_image, (30,30))
+        self.rect = self.image.get_rect()
+        self.rect.center = (width // 2, height - 60)
+        self.direction = 0 #left = 0, right = 1
+        self.speed = 30
 
 # Start the Loop of game
 done = False
